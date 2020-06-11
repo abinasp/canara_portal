db5f50231fde71e538f57eb256bfe627afd31774
Abinas Patra <2010abinas@gmail.com>
2020-06-11 12:25:55 +0530

    Translator table

--- ui/src/pages/dashboard/styles.js
+++ ui/src/pages/dashboard/styles.js
@@ -145,6 +145,35 @@ const DashboardStyle = theme => ({
       }
     }
   },
+
+  widgetWrapper: {
+    display: "flex",
+    minHeight: "100%",
+  },
+  widgetHeader: {
+    padding: theme.spacing(3),
+    paddingBottom: theme.spacing(1),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  widgetRoot: {
+    boxShadow: theme.customShadows.widget,
+  },
+  widgetBody: {
+    paddingBottom: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    paddingLeft: theme.spacing(3),
+  },
+  noPadding: {
+    padding: 0,
+  },
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    flexGrow: 1,
+    overflow: "hidden",
+  },
 });
 
 export default DashboardStyle;
--- ui/src/pages/dashboard/translator/translator.js
+++ ui/src/pages/dashboard/translator/translator.js
@@ -5,17 +5,19 @@ import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,
   TableFooter,
   TablePagination,
   Paper,
   TableHead,
   TableRow,
   TextField,
-  Button
+  Button,
+  InputAdornment
 } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 import { connect } from 'react-redux';
+import ReactSelect from 'react-select';
 import classNames from 'classnames';
 
 import { withStyles } from '@material-ui/core/styles';
@@ -96,15 +98,33 @@ class TranslatorDashboard  extends React.Component{
     this.state={
       unModeratedStrings: [],
       page: 0,
-      rowsPerPage: 5
+      rowsPerPage: 5,
+      search:'',
+      selectedLanguage: [],
+      isLanguageChecked: false,
+      languageLists: [],
+      menuOpenLang: false,
     }
     this.initSwalekh = this.initSwalekh.bind(this);
     this.handleChangePage = this.handleChangePage.bind(this);
     this.handleChangeRowsPerPage = this.handleChangeRowsPerPage.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
 
   componentDidMount(){
-    this.setState({ unModeratedStrings: dummy });
+    let newOptions = [];
+    const { user } =this.props;
+    if(user && user.languages && user.languages.length > 0){
+      user.languages.forEach(lang=>{
+        if (lang !== 'english') {
+          newOptions.push({
+            label: lang[0].toUpperCase() + lang.slice(1),
+            value: lang
+          });
+        }
+      });
+    }
+    this.setState({ unModeratedStrings: dummy, languageLists: newOptions, selectedLanguage: newOptions });
   }
 
   componentWillReceiveProps(nextProps){
@@ -123,6 +143,10 @@ class TranslatorDashboard  extends React.Component{
       const { value } = e.target;
       this.setState({ rowsPerPage: value });
     }
+  }
+
+  handleSearchChange(e){
+    this.setState({search: e.target.value})
   }
 
   handleChangeTarget(e,i){
@@ -156,56 +180,105 @@ class TranslatorDashboard  extends React.Component{
 
   render(){
     const { classes, theme, user } = this.props;
-    const { unModeratedStrings, page, rowsPerPage } =this.state;
-    console.log(unModeratedStrings)
+    const { unModeratedStrings, page, rowsPerPage, search } =this.state;
     return(
       <>
         <Grid item xs={12}>
-          <Table style={{ marginTop: '-10px' }}>
-            <TableHead>
-              <TableRow>
-                <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Domain</TableCell>
-                <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Source String</TableCell>
-                <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Target String</TableCell>
-                <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Target Language</TableCell>
-                <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {unModeratedStrings && unModeratedStrings.map((s,i)=>(
-                <TableRow key={i}>
-                  <TableCell padding="default" align="center">{s.apikey}</TableCell>
-                  <TableCell padding="default" align="center">{s.source}</TableCell>
-                  <TableCell padding="default" align="center">
-                    <TextField 
-                      id={`target_${i}`}
-                      value={s.target}
-                      style={{ width: '100%'}}
-                      onChange={(e)=>this.handleChangeTarget(e,i)}
-                      onFocus={()=>this.initSwalekh(i)}
+          <Paper className={classes.paper} classes={{root: classes.widgetRoot}}>
+            <Grid container xs={12} style={{ padding: '24px' }}>
+              <Grid item xs={6}>
+                {user && user.languages && user.languages.length>0 && (
+                  <div style={{ width: '50%' }}>
+                    <ReactSelect
+                      isMulti
+                      options={this.state.languageLists}
+                      menuIsOpen={this.state.menuOpenLang}
+                      onFocus={() => this.setState({ menuOpenLang: true })}
+                      onBlur={() => this.setState({ menuOpenLang: false })}
+                      value={this.state.selectedLanguage}
+                      tabSelectsValue={false}
+                      onChange={e=>{
+                        if(!e){
+                          this.setState({ selectedLanguage: [] }, ()=>{
+                            if(!this.state.selectedLanguage || this.state.selectedLanguage.length === 0 || this.state.selectedLanguage.length !== this.state.languageLists.length){
+                              this.setState({ isLanguageChecked: false });
+                            }
+                          })
+                        }else{
+                          this.setState({ selectedLanguage: e }, ()=>{
+                            if (!this.state.selectedLanguage || this.state.selectedLanguage.length === 0 || this.state.selectedLanguage.length !== this.state.languageLists.length) {
+                              this.setState({ isLanguageChecked: false });
+                            }
+                          })
+                        }
+                      }}
                     />
-                  </TableCell>
-                  <TableCell padding="default" align="center">{s.targetLanguage}</TableCell>
-                  <TableCell padding="default" align="center">
-                    <Button color="primary" variant="contained" onClick={()=>this.onTargetSave(i)} >Save</Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TablePagination 
-                  rowsPerPageOptions={[5,10,25,50,100]}
-                  count={unModeratedStrings.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  onChangePage={this.handleChangePage}
-                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                  ActionsComponent={TablePaginationsAction}
+                  </div>
+                )}
+              </Grid>
+              <Grid item xs={6}>
+                <TextField 
+                  className={classes.margin}
+                  value={search}
+                  style={{ width: '50%', float:'right' }}
+                  placeholder="Search Source..."
+                  onChange={this.handleSearchChange}
+                  InputProps={{
+                    startAdornment:(
+                      <InputAdornment position="start">
+                        <SearchIcon color="primary" />
+                      </InputAdornment>
+                    )
+                  }}
                 />
-              </TableRow>
-            </TableFooter>
-          </Table>
+              </Grid>
+            </Grid>
+            <Table style={{ marginTop: '-10px' }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Domain</TableCell>
+                  <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Source String</TableCell>
+                  <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Target String</TableCell>
+                  <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Target Language</TableCell>
+                  <TableCell padding="default" align="center" style={{ color: '#4581A8'}}>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {unModeratedStrings && unModeratedStrings.map((s,i)=>(
+                  <TableRow key={i}>
+                    <TableCell padding="default" align="center">{s.apikey}</TableCell>
+                    <TableCell padding="default" align="center">{s.source}</TableCell>
+                    <TableCell padding="default" align="center">
+                      <TextField 
+                        id={`target_${i}`}
+                        value={s.target}
+                        style={{ width: '100%'}}
+                        onChange={(e)=>this.handleChangeTarget(e,i)}
+                        onFocus={()=>this.initSwalekh(i)}
+                      />
+                    </TableCell>
+                    <TableCell padding="default" align="center">{s.targetLanguage}</TableCell>
+                    <TableCell padding="default" align="center">
+                      <Button color="primary" variant="contained" onClick={()=>this.onTargetSave(i)} >Save</Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination 
+                    rowsPerPageOptions={[5,10,25,50,100]}
+                    count={unModeratedStrings.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    ActionsComponent={TablePaginationsAction}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </Paper>
         </Grid>
       </>
     )
